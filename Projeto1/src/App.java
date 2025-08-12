61ca4af6439f5a775cb531759ac1357c123330fa
Cauê <72117913+cauedasilva@users.noreply.github.com>
2025-08-12 19:38:38 -0300

    Add files via upload

---
+++ Projeto1/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 15;
+        int valor = num1 + num2;
+        System.out.println("A soma é: " + valor);
+
+        int subtracao = num2 - num1;
+        int mult = num1 * num2;
+
+        System.out.println("A subtração é: " + subtracao + " e a multiplicação é: " + mult);
+
+    }
+}
