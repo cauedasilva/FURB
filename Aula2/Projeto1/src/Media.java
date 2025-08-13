b0c61eb50e1c488ac624114c80b088f377c4143d
Cauê <cauedasilva.slv@gmail.com>
2025-08-13 00:08:44 -0300

    modified:   Aula2/Projeto1/src/Media.java commit

--- Aula2/Projeto1/src/Media.java
+++ Aula2/Projeto1/src/Media.java
@@ -7,6 +7,21 @@ public class Media {
         Scanner teclado = new Scanner(System.in);
         System.out.println("Digite o nome do aluno: ");
         String nome = teclado.nextLine();
-        System.out.println(nome);
+        System.out.println("Digite a primeira nota: ");
+        double nota1 = teclado.nextDouble();
+        System.out.println("Digite a segunda nota: ");
+        double nota2 = teclado.nextDouble();   
+        System.out.println("Digite a terceira nota: ");
+        double nota3 = teclado.nextDouble();
+        double media = (nota1 + nota2 + nota3) / 3;
+        System.out.printf("A média do aluno %s é: %.2f%n", nome, media);
+        teclado.close();
+        if (media >= 6) {
+            System.out.println("Aluno aprovado!");
+        } else if (media >= 5) {
+            System.out.println("Aluno em recuperação!");
+        } else {
+            System.out.println("Aluno reprovado!");
+        }
     }
 }
