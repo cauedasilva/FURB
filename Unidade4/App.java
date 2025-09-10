f7215aab30dd62d824f27a627069c27c273583fb
Cauê <72117913+cauedasilva@users.noreply.github.com>
2025-09-09 21:34:01 -0300

    Add files via upload

---
+++ Unidade4/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) {
+        boolean sinalVerde = true;
+        if (sinalVerde) {
+            System.out.println("Pode atravessar");
+        } else {
+            System.out.println("Não pode atravessar");
+        }
+
+        int idade = 18;
+        if (idade >= 18) {
+            System.out.println("Pode dirigir");
+        } else {
+            System.out.println("Não pode dirigir");
+        }
+    }
+}
