9a5f0ca5a105f7206b1d679e680513f149ad19b2
Cauê <72117913+cauedasilva@users.noreply.github.com>
2025-10-21 19:57:36 -0300

    Update Uni5Exe11.java

--- Unidade5/Exercicios/Uni5Exe11.java
+++ Unidade5/Exercicios/Uni5Exe11.java
@@ -1,16 +1,18 @@
 public class Uni5Exe11 {
-    int num = 1;
+    int num = 1, total;
 
     public Uni5Exe11() {
         exibirResultado();
     }
 
     private void exibirResultado() {
-        for (int i = 1; i < 16; i++) {
+        for (int i = 0; i < 16; i++) {
+            total = total + num;
             num = num * 3;
+            System.out.println(total);
         }
 
-        System.out.println("o total é: " + num);
+        System.out.println("o total é: " + total);
     }
 
     public static void main(String[] args) {
