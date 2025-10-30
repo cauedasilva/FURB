e582171de5905d45a5c18e53a416ff50f202dff9
Cauê <cauedasilva.slv@gmail.com>
2025-10-30 15:42:32 -0300

    modified:   Unidade5/Exercicios/Uni5Exe31.java 	modified:   Unidade5/Exercicios/Uni5Exe33.java

--- Unidade5/Exercicios/Uni5Exe31.java
+++ Unidade5/Exercicios/Uni5Exe31.java
@@ -2,21 +2,32 @@ import java.util.Scanner;
 
 public class Uni5Exe31 {
     Scanner sc = new Scanner(System.in);
-    int n;
+    int num, divisor = 2;
 
     public Uni5Exe31() {
         inserirValor();
-        exibirResultado();
     }
 
     private void inserirValor() {
         System.out.println("Informe um número inteiro positivo: ");
-        n = sc.nextInt();
-        
-    }
+        num = sc.nextInt();
+        System.out.print("Decomposição de " + num + ": ");
 
-    private void exibirResultado() {
-        
+        while (divisor * divisor <= num) {
+            while (num % divisor == 0) {
+                System.out.print(divisor);
+                num /= divisor;
+
+                if (num > 1) {
+                    System.out.print(" x ");
+                }
+            }
+            divisor++;
+        }
+
+        if (num > 1) {
+            System.out.print(num);
+        }
     }
 
     public static void main(String[] args) {
--- Unidade5/Exercicios/Uni5Exe33.java
+++ Unidade5/Exercicios/Uni5Exe33.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class Uni5Exe33 {
     Scanner sc = new Scanner(System.in);
-    int n;
+    int num, votosUm, votosDois, votosTres, votosQuatro, votosNulo, votosBranco, totalVotos, totalBrancosNulos;
+    double percentualBrancosNulos;
 
     public Uni5Exe33() {
         inserirValor();
@@ -10,13 +11,53 @@ public class Uni5Exe33 {
     }
 
     private void inserirValor() {
-        System.out.println("Informe um número inteiro positivo: ");
-        n = sc.nextInt();
+        do {
+            System.out.println("Informe o número do voto: (0 para encerrar) ");
+            num = sc.nextInt();
+            if (num == 1 || num == 2 || num == 3 || num == 4 || num == 5 || num == 6) {
+                elencarVotos();
+            } else {
+                System.out.println("Opção incorreta");
+            }
+        } while (num != 0);
         
     }
 
+    private void elencarVotos() {
+        switch (num) {
+            case 1:
+                votosUm++;
+                break;
+            case 2:
+                votosDois++;
+                break;
+            case 3:
+                votosTres++;
+                break;
+            case 4:
+                votosQuatro++;
+                break;
+            case 5:
+                votosNulo++;
+                break;
+            case 6:
+                votosBranco++;
+                break;
+        }
+
+        totalVotos++;
+        totalBrancosNulos = votosNulo + votosBranco;
+        percentualBrancosNulos = (double) (totalBrancosNulos * 100.0) / (double) totalVotos;
+    }
+
     private void exibirResultado() {
-        
+        System.out.println("Total de votos para o candidato 1: " + votosUm);
+        System.out.println("Total de votos para o candidato 2: " + votosDois);
+        System.out.println("Total de votos para o candidato 3: " + votosTres);
+        System.out.println("Total de votos para o candidato 4: " + votosQuatro);
+        System.out.println("Total de votos nulos: " + votosNulo);
+        System.out.println("Total de votos em branco: " + votosBranco);
+        System.out.println("Percentual de votos brancos e nulos: " + percentualBrancosNulos + "%");
     }
 
     public static void main(String[] args) {
