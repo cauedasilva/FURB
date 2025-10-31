928da93925a0a0a17e735305163f918f29d9625f
Cauê <cauedasilva.slv@gmail.com>
2025-10-30 23:52:19 -0300

    modified:   Unidade5/Exercicios/Uni5Exe34.java

--- Unidade5/Exercicios/Uni5Exe34.java
+++ Unidade5/Exercicios/Uni5Exe34.java
@@ -2,21 +2,53 @@ import java.util.Scanner;
 
 public class Uni5Exe34 {
     Scanner sc = new Scanner(System.in);
-    int n;
+    int opcao, contadorContas = 0;
+    double total;
+    boolean continuar = true;
 
     public Uni5Exe34() {
         inserirValor();
-        exibirResultado();
     }
 
     private void inserirValor() {
-        System.out.println("Informe um número inteiro positivo: ");
-        n = sc.nextInt();
+        while (continuar) {
+            System.out.println("(1) Encerrar a conta de um hóspede");
+            System.out.println("(2) Verificar o número de contas encerradas");
+            System.out.println("(3) Sair");
+            opcao = sc.nextInt();
+            switch (opcao) {
+                case 1:
+                    calculcarConta();
+                    break;
+                case 2:
+                    System.out.println("O número de contas encerradas é: " + contadorContas);
+                    break;
+                case 3:
+                    continuar = false;
+                    break;
+                default:
+                    System.out.println("Opção inválida.");
+            }
+        }
         
     }
 
-    private void exibirResultado() {
+    private void calculcarConta() {
+        System.out.println("Informe o nome do hóspede: ");
+        String nomeHospede = sc.next();
+        System.out.println("Informe o número de diárias: ");
+        int diarias = sc.nextInt();
         
+        if (diarias < 15) {
+            total = diarias * 7.50;
+        } else if (diarias == 15) {
+            total = diarias * 6.50;
+        } else {
+            total = diarias * 5.00;
+        }
+
+        System.out.printf("A conta do hóspede %s é: R$ %.2f%n", nomeHospede, total);
+        contadorContas++;
     }
 
     public static void main(String[] args) {
