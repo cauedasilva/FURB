ec95df0faab6c7475c2190870d24e287f4981ee0
Cauê <72117913+cauedasilva@users.noreply.github.com>
2025-11-11 20:54:59 -0300

    Add files via upload

---
+++ Unidade6/Exercicios/Uni6Exe09.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Uni6Exe09 {
+    Scanner sc = new Scanner(System.in);
+    boolean continuar = true;
+
+    public Uni6Exe09() {
+        inserirValores();
+    }
+
+    private void inserirValores() {
+        
+    }
+
+    private void exibirResultado() {
+    }
+
+    public static void main(String[] args) {
+        new Uni6Exe09();
+    }
+}
