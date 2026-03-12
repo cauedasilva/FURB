fd16df4aed25c8c48737b51d50eea388a158d072
Cauê <cauedasilva.slv@gmail.com>
2026-03-11 23:41:12 -0300

    .

--- 2026_1/Unidade1/lista03/que02/src/main/java/com/caue/Main.java
+++ 2026_1/Unidade1/lista03/que02/src/main/java/com/caue/Main.java
@@ -1,13 +1,25 @@
 package com.caue;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Funcionario[] funcionarios = new Funcionario[5];
 
         for (int i = 0; i < funcionarios.length; i++) {
             System.out.println("Informe o nome do funcionário Nº" + i + ": ");
-            
+            funcionarios[i].setNome(scanner.next());
+            System.out.println("Informe o salário do funcionário Nº" + i + ": ");
+            funcionarios[i].setSalario(scanner.nextDouble());
         }
-        
+
+        for (int i = 0; i < funcionarios.length; i++) {
+            System.out.println("Nome: " + funcionarios[i].getNome());
+            System.out.println("Salário: " + funcionarios[i].getSalario());
+            System.out.println("Imposto: " + funcionarios[i].calcularIrpf());
+        }
+
+        scanner.close();
     }
 }
